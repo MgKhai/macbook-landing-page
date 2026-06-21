548bb7aacd65de9ee7e04aeaf74e82942b9547bf
MgKhai <195129227+MgKhai@users.noreply.github.com>
2026-06-21 09:47:58 +0630

    first_commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  return (
+      <div>
+          <h1 className="text-red-500">Red</h1>
+          <h1 className="text-green-500">Green</h1>
+          <h1 className="text-blue-500">Blue</h1>
+      </div>
+  );
+}
+
+export default App;
