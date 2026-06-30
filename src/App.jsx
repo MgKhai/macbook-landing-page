97a785e2e601812f9c013fad228b817352b268e0
MgKhai <195129227+MgKhai@users.noreply.github.com>
2026-06-30 11:07:09 +0630

    feat:productViewer_added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import ProductViewer from "./components/ProductViewer";
 
 function App() {
   return (
     <main className="bg-black min-h-screen">
       <NavBar />
       <Hero />
+      <ProductViewer />
     </main>
   );
 }
